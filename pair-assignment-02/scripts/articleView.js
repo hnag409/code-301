d1eb8c939761078f142dec9f37f273888b4abfd1
Yvonne Hayes <queyvonne@gmail.com>
2016-04-12 14:22:27 -0700

    hiding all authors and fading in just the chosen one

--- pair-assignment-02/scripts/articleView.js
+++ pair-assignment-02/scripts/articleView.js
@@ -28,10 +28,16 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
+  var authorName = $(this).val(); //Turned value of author into a variable +++++++++
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      // DONE: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+
+      $('article').hide(); //hiding ALL articles
+      $('article[data-attribute="' + authorName +'"]').fadeIn('slow'); //fade in JUST the one author
+
+
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should

de99a48b03817bf385da44aad8fad9d82d244180
goodwid <goodwid@gmail.com>
2016-04-15 11:29:14 -0700

    Added event handler and hid JSON preview field.

--- pair-assignment-05/scripts/articleView.js
+++ pair-assignment-05/scripts/articleView.js
@@ -69,14 +69,31 @@ articleView.initNewArticlePage = function() {
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we
   // have data to export. Also, let's add focus event to help us select the JSON.
+  $('#export-field').hide();
+
+
+
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  $('#new-form').on('focus input','input, textarea', articleView.create);
+
+
 };
 
-articleView.create = function() {
+//
+
+articleView.create = function(e) {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
+  // var newArticle = {
+  //   title:       '',
+  //   category:    '',
+  //   author:      '',
+  //   authorUrl:   '',
+  //   publishedOn: '',
+  //   body:        ''
+  // }
+  // $('#articles').html()
   // TODO: Instantiate an article based on what's in the form fields:
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
@@ -84,6 +101,7 @@ articleView.create = function() {
   // TODO: Activate the highlighting of any code blocks:
 
   // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  // $JsonArticleContent = (JSON.stringify($('#article-body').val());
 };
 
 

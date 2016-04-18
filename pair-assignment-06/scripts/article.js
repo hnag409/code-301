7df00d387be9a621d54fc78f274651e43ed45713
Johnny Luangphasy <johnny@jluangphasy.com>
2016-04-18 15:51:10 -0700

    Removed comments

--- pair-assignment-06/scripts/article.js
+++ pair-assignment-06/scripts/article.js
@@ -7,11 +7,6 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-// DONE: Instead of a global `articles = []` array, let's track this list of all articles directly on
-// Article <-- the **constructor function**. Note: ".all" is NOT on the prototype. In JS, functions are
-// objects; you can add properties to any function, at any time. In this app, we have an array of Article
-// objects that we want to build; that array does not belong in Article.prototype since that array is
-// "larger" than any single Article.
 Article.all = [];
 
 Article.prototype.toHtml = function() {
@@ -24,13 +19,7 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// DONE: There are some other functions that also relate to articles across the board, rather than
-// just single instances. Object-oriented programming would call these "class-level" functions,
-// that are relevant to the entire "class" of objects that are Articles.
-
-// DONE: This function will take the rawData, how ever it is provided,
-// and use it to instantiate all the articles. This code is moved from elsewhere, and
-// encapsulated in a simply-named function for clarity.
+// Take the rawData, instantiate all the articles
 Article.loadAll = function(rawData) {
   rawData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -41,41 +30,27 @@ Article.loadAll = function(rawData) {
   });
 };
 
-// This function will retrieve the data from either a local or remote source,
+// Retrieve data from either a local or remote source,
 // and process it, then hand off control to the View.
 Article.fetchAll = function() {
   $.ajax({
     url: '/data/ipsumArticles.json',
     dataType: 'json',
-    // cache: true,
-    // ifModified: true,
     method: 'HEAD'
   }).done(function(data, textStatus, request) {
     var eTag = request.getResponseHeader('eTag');
 
     if (eTag == localStorage.eTag && localStorage.rawData) {
-      // When rawData is already in localStorage,
-      // we can load it by calling the .loadAll function,
-      // and then render the index page (using the proper method on the articleView object).
-      // DONE: What do we pass in here to the .loadAll function?
       Article.loadAll(JSON.parse(localStorage.rawData));
-      // DONE: Change this fake method call to the correct one that will render the index page.
+
       articleView.initIndexPage();
     } else {
-      // DONE: When we don't already have the rawData in local storage, we need to get it from the JSON file,
-      //       which simulates data on a remote server. Run live-server or pushstate-server!
-      //       Please do NOT browse to your HTML file(s) using a "file:///" link. RUN A SERVER INSTEAD!!
-
-      // Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
       $.getJSON('/data/ipsumArticles.json').done(function (data, textStatus, request) {
-        // Store the resulting JSON data with the .loadAll method,
         Article.loadAll(data);
 
-        // Cache the data in localStorage so next time we won't enter this "else" block (avoids hitting the server),
         localStorage.rawData = JSON.stringify(data);
         localStorage.eTag = request.getResponseHeader('eTag');
 
-        // Render the index page (perhaps with an articleView method?).
         articleView.initIndexPage();
       });
     }
